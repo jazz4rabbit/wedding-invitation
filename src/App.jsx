384e4308538f6d5d0a68ec803ee9f85e3890062a
jazz4rabbit <jazz4rabbit@gmail.com>
2024-06-24 01:16:40 +0900

    image test

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
+        <img src="main.jpg" alt="img" />
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
